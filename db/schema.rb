65cbc2d83946a39e14da740642a51f143a675998
JMKelley <jonathan.mark.kelley@gmail.com>
2015-03-14 22:15:48 +0000

    added admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314215546) do
+ActiveRecord::Schema.define(version: 20150314220807) do
 
   create_table "impressions", force: true do |t|
     t.string   "impressionable_type"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20150314215546) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
