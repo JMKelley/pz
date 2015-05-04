a051e271e0763b81ad13b0a7cc3d33c7970255a6
JMKelley <jonathan.mark.kelley@gmail.com>
2015-05-04 12:59:12 +0000

    Added Description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150419144916) do
+ActiveRecord::Schema.define(version: 20150504125634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150419144916) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "slug"
+    t.string   "description"
   end
 
   add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
